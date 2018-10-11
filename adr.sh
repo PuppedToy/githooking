073d8176cb8b1b438cb391be988ace9091d2fa3a
PuppedToy <mapastzar@hotmail.com>
2018-10-11 16:28:25 +0200

    ajhdssad

--- adr.sh
+++ adr.sh
@@ -4,7 +4,15 @@ files=$(ls *.rndtxt 2> /dev/null)
 rndn=$RANDOM
 let "rndn %= 4"
 
-if [ "$files" != "" ] || [ $rndn -le 2 ] || [ "$1" == "-m" ] && [ "$1" != "-n" ]; then
+if [ "$1" == "-m" ]; then
+	rndn=0
+elif [ "$1" == "-n" ]; then
+	rndn=3
+fi
+
+echo $rndn
+
+if [ "$files" != "" ] && [ $rndn -le 2 ]; then
 	nfiles=$(echo $files | wc -w | xargs)
 	rndn=$RANDOM
 	let "rndn %= $nfiles"
