dcf9a2d22fdfaa161db06133f8cb5f2b44d5b205
PuppedToy <mapastzar@hotmail.com>
2018-10-11 16:23:24 +0200

    añsjd

--- adr.sh
+++ adr.sh
@@ -4,18 +4,18 @@ files=$(ls *.rndtxt 2> /dev/null)
 rndn=$RANDOM
 let "rndn %= 4"
 
-if [ "$files" == "" ] || [ $rndn -gt 2 ]; then
-	new=$(node add_random_change 8)".rndtxt"
-	echo $(node add_random_change) > $new
-	echo "New file! --> $new"
-else
+if [ "$files" != "" ] || [ $rndn -le 2 ] || [ "$1" == "-m" ] && [ "$1" != "-n" ]; then
 	nfiles=$(echo $files | wc -w | xargs)
 	rndn=$RANDOM
 	let "rndn %= $nfiles"
 	((rndn++))
 	old=$(echo $files | tr " " "\n"|sed -n "${rndn}p")
 	echo $(node add_random_change) >> $old
-	echo "Old file! --> $old"
+	echo "Modified file: $old"
+else
+	new=$(node add_random_change 8)".rndtxt"
+	echo $(node add_random_change) > $new
+	echo "Created file: $new"
 fi
 	
 
